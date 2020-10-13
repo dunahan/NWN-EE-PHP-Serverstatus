23dd9b0bf3bf9021f77a38f0fb85b2d76004ccd1
dunahan <25346708+dunahan@users.noreply.github.com>
2020-10-13 09:08:44 +0200

    Update fulchk.php
    Added some of the if/else statements that were missing at first commit

--- fulchk.php
+++ fulchk.php
@@ -1,26 +1,70 @@
 <?php
-  $_url = 'https://api.nwn.beamdog.net/v1/servers/[ADD_YOUR_PUBLIC_KEY_HERE]';
+  $url = 'https://api.nwn.beamdog.net/v1/servers/';  // add your kx_pk key after the last slash
 
-  $_url_var = implode('', file($_url));
-  $_url_obj = json_decode($_url_var, true);
+  $urlvar = implode('', file($url));
+  $urlobj = json_decode($urlvar, true);
+  
+  $onl = $urlobj['last_advertisement'];
+  if ($onl > 1) {
+    echo 'Server is Online<br>';
+  }
+  else {
+    echo 'Server is Offline<br>';
+  }
+  
+  echo 'Server Name: '.$urlobj['session_name'].'<br>'; 
+  echo 'Players Online: '.$urlobj['current_players'].'/'.$urlobj['max_players'].'<br>';
+  echo 'Module Name: '.$urlobj['module_name'].'<br>';
+  echo 'Server Host: '.$urlobj['host'].':'.$urlobj['port'].'<br>';
+  echo 'Server Country: '.$urlobj['language'].'<br>';                             //uk/de/us
+  echo 'Server Description: '.$urlobj['module_description'].'<br>';
+  echo 'OS: '.$urlobj['os'].'<br>';                                               //win/lnx/...
+  echo 'Player vs Player: '.$urlobj['pvp'].'<br>';                                //party/...
+  echo 'Game Category: '.$urlobj['game_type'].'<br>';                             //roleplay/...
+  echo 'Level Range: '.$urlobj['min_level'].'-'.$urlobj['max_level'].'<br>';
+  echo 'Server Version: '.$urlobj['build'].'.'.$urlobj['rev'].'<br>';
+  
+  $vlt = $urlobj['servervault'];
+  if ($vlt = 'true') {
+    echo 'Characters Vault: Server<br>';
+  }
+  else {
+    echo 'Characters Vault: Local<br>';
+  }
+  
+  echo 'Party Type: '.$urlobj['one_party'].'<br>';                                //type
+  
+  $plp = $urlobj['player_pause'];
+  if ($plp = 'true') {
+    echo 'Players Pause: Yes<br>';
+  }
+  else {
+    echo 'Players Pause: No<br>';
+  }
 
-  echo 'Server is '.'<br>';                                                         //online/offline
-  echo 'Server Name: '.$_url_obj['session_name'].'<br>'; 
-  echo 'Players Online: '.$_url_obj['current_players'].'/'.$_url_obj['max_players'].'<br>';
-  echo 'Module Name: '.$_url_obj['module_name'].'<br>';
-  echo 'Server Host: '.$_url_obj['host'].':'.$_url_obj['port'].'<br>';
-  echo 'Server Country: '.$_url_obj['language'].'<br>';                             //uk/de/us
-  echo 'Server Description: '.$_url_obj['module_description'].'<br>';
-  echo 'OS: '.$_url_obj['os'].'<br>';                                               //win/lnx
-  echo 'Player vs Player: '.$_url_obj['pvp'].'<br>';                                //party/usw
-  echo 'Game Category: '.$_url_obj['game_type'].'<br>';                             //roleplay/usw
-  echo 'Level Range: '.$_url_obj['min_level'].'-'.$_url_obj['max_level'].'<br>';
-  echo 'Server Version: '.$_url_obj['build'].'.'.$_url_obj['rev'].'<br>';
-  echo 'Characters Vault: '.$_url_obj['servervault'].'<br>';                        //server/local
-  echo 'Party Type: '.$_url_obj['one_party'].'<br>';                                //type
-  echo 'Players Pause: '.$_url_obj['player_pause'].'<br>';                          //yes/no
-  echo 'ILR: '.$_url_obj['ilr'].'<br>';                                             //yes/no
-  echo 'Legal Characters: '.$_url_obj['elc'].'<br>';                                //yes/no
-  echo 'Password: '.$_url_obj['passworded'].'<br>';                                 //yes/no
-  echo 'Last check: '.$_url_obj['first_seen'].'-'.$_url_obj['first_seen'].'<br>';   //calc diff
+  $ilr = $urlobj['ilr'];
+  if ($ilr = 'false') {
+    echo 'Item Level Restrictions: No<br>';
+  }
+  else {
+    echo 'Item Level Restrictions: Yes<br>';
+  }
+  
+  $elc = $urlobj['elc'];
+  if ($elc = 'false') {
+    echo 'Legal Characters: No<br>';
+  }
+  else {
+    echo 'Legal Characters: Yes<br>';
+  }
+  
+  $pwd = $urlobj['passworded'];
+  if ($pwd = 'false') {
+    echo 'Password: No<br>';
+  }
+  else {
+    echo 'Password: Yes<br>';
+  }
+  
+  echo 'Last check: '.$urlobj['first_seen'].'-'.$urlobj['last_advertisement'].'<br>';   //calc diff
 ?>
